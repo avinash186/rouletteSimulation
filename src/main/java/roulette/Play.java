f69a7000e93f52434e4ac5bfde458bf5c91b1e48
Avinash Raya <nraya@scholastic.com>
2023-06-10 14:33:28 -0400

    I Am Your Father

--- src/main/java/roulette/Play.java
+++ src/main/java/roulette/Play.java
@@ -6,7 +6,7 @@ public class Play {
     public static ColorBet colorbet = new ColorBet();
     private static int earnings = 0;
     public static void play(){
-        earnings += colorbet.red(10,2);
+        earnings += colorbet.red(10,1);
         System.out.println(earnings);
     }
 }
